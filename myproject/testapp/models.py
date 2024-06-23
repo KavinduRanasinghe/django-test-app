8afc4269757784119178220d3a2419a6ccd4c71a
KavinduRanasinghe <pehesara2001@gmail.com>
2024-06-23 08:22:23 +0530

    feat: Create models and migrations for myapp

--- myproject/testapp/models.py
+++ myproject/testapp/models.py
@@ -1,3 +1,5 @@
-from django.db import models
-
-# Create your models here.
+from django.db import models 
+class Person(models.Model): 
+    name = models.CharField(max_length=20) 
+    email = models.EmailField() 
+    phone = models.CharField(max_length=20) 
